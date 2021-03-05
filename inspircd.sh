51d10218bbb930854de0161bbaf343c24c9df770
Sadie Powell <sadie@witchery.services>
2021-03-05 14:55:45 +0000

    Add the initial server configs.

---
+++ inspircd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ROOT=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
+if [ $# -eq 0 ]
+then
+	echo "<die reason=\"You did not specify a server argument to inspircd.sh!\">"
+	exit 0
+elif [ ! -f "${ROOT}/servers/$1.conf" ]
+then
+	echo "<die reason=\"The server argument you specified for inspircd.sh is not valid!\">"
+	exit 0
+fi
+
+CONFIG_DATE=$(git log --format=%cd --max-count=1 2>/dev/null)
+CONFIG_REV=$(git describe --always --dirty 2>/dev/null)
+echo "<alias text=\"CONFREV\" replace=\"NOTICE \$nick :Configuration revision: $CONFIG_REV ($CONFIG_DATE)\">"
+echo "<include file=\"servers/$1.conf\">"
+echo "<include directory=\"common\">"
